dab18a84141531d203efcf06408f8c0864c13e0f
yuval rachaman <sd115yuv@gmail.com>
2023-10-25 15:06:27 +0300

    ads

---
+++ scripts/index.js
@@ -0,0 +1,39 @@
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+  },
+];
+
+const PenEdit = document.querySelector("#PenEdit");
+const ModalEdit = document.querySelector("#ModalEdit");
+const MoalClose = document.querySelector(".modal__close");
+function closePopup() {
+  ModalEdit.classList.remove("modal_opened");
+}
+PenEdit.addEventListener("click", () => {
+  ModalEdit.classList.add("modal_opened");
+});
+MoalClose.addEventListener("click", () => {
+  closePopup();
+});
