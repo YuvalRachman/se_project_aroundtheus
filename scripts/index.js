b897047d430be4d1aa34b2b60cad274383fefd4b
yuval rachaman <sd115yuv@gmail.com>
2023-11-27 08:12:26 +0200

    fix-1

--- scripts/index.js
+++ scripts/index.js
@@ -24,16 +24,48 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
+const penEdit = document.querySelector("#penBth");
+const modalEdit = document.querySelector("#modalEdit");
+const modalClose = document.querySelector(".modal__close");
+const profileTitle = document.querySelector(".profile__title");
+const profileSubtitle = document.querySelector(".profile__subtitle");
+const profilleTitleInpot = document.querySelector("#profile-title-inpot");
+const profilleSubtitleInpot = document.querySelector("#profile-subtitle-inpot");
 
-const PenEdit = document.querySelector("#PenEdit");
-const ModalEdit = document.querySelector("#ModalEdit");
-const MoalClose = document.querySelector(".modal__close");
-function closePopup() {
-  ModalEdit.classList.remove("modal_opened");
+const profileEditForm = modalEdit.querySelector(".modal__form");
+const cardList = document.querySelector(".cards__list");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
+function getCsrdElement(cardData) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImageEL = cardElement.querySelector(".card__image");
+  const cardTitleEL = cardElement.querySelector(".card__title");
+  cardTitleEL.textContent = cardData.name;
+  cardImageEL.src = cardData.link;
+  cardImageEL.alt = cardData.name;
+  return cardElement;
 }
-PenEdit.addEventListener("click", () => {
-  ModalEdit.classList.add("modal_opened");
+function closePopup() {
+  modalEdit.classList.remove("modal_opened");
+}
+
+penEdit.addEventListener("click", () => {
+  profilleTitleInpot.value = profileTitle.textContent;
+  profilleSubtitleInpot.value = profileSubtitle.textContent;
+  modalEdit.classList.add("modal_opened");
 });
-MoalClose.addEventListener("click", () => {
+modalClose.addEventListener("click", () => {
   closePopup();
+});
+
+function handleProfileEditSubmit(e) {
+  e.preventDefault();
+  profileTitle.textContent = profilleTitleInpot.value;
+  profileSubtitle.textContent = profilleSubtitleInpot.value;
+  closePopup();
+}
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+initialCards.forEach((cardData) => {
+  const cardElement = getCsrdElement(cardData);
+  cardList.append(cardElement);
 });
