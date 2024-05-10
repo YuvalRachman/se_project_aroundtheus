1d5c649566080f0538b80cb2885422d3fbb9b593
yuval rachaman <sd115yuv@gmail.com>
2024-05-10 14:59:19 +0300

    delte card work

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,31 @@
+import Api from "../API/Api.js";
+
 export default class Card {
-  constructor({ name, link }, handleImageClick, cardSelector) {
+  constructor({ name, link, _id }, handleImageClick, cardSelector) {
     this._name = name;
     this._link = link;
+    this._cardId = _id;
     this._cardSelector = cardSelector;
+    console.log(this._cardId);
     this._handleImageClick = handleImageClick;
     this._element = this._getTemplate(); // Initialize _element once
     this._likeButton = this._element.querySelector(".card__like-button"); // Initialize _likeButton once
     this._cardImage = this._element.querySelector(".card__image"); // Initialize _cardImage once
     this._trashButton = this._element.querySelector(".card__trash-button"); // Initialize _trashButton once
 
-    this._setEventListeners(); // Call _setEventListeners once
+    this._setEventListeners();
   }
 
   _setEventListeners() {
     this._likeButton.addEventListener("click", this._handleLike);
     this._trashButton.addEventListener("click", () => this._deleteCard());
+
     this._cardImage.addEventListener("click", () =>
-      this._handleImageClick({ link: this._link, name: this._name })
+      this._handleImageClick({
+        link: this._link,
+        name: this._name,
+        id: this._cardId,
+      })
     );
   }
 
@@ -25,10 +34,26 @@ export default class Card {
   };
 
   _deleteCard() {
-    this._element.remove();
-    this._element = null;
-  }
+    // Instantiate Api class
+    const api = new Api({
+      baseUrl: `https://around-api.en.tripleten-services.com/v1`,
+      headers: {
+        authorization: "59d50a7d-5cbc-4141-92cc-d4a1e1821930",
+        "Content-Type": "application/json",
+      },
+    });
 
+    // Call deleteCard method of the Api class
+    api
+      .deleteCard(this._cardId)
+      .then(() => {
+        this._element.remove();
+        this._element = null;
+      })
+      .catch((error) => {
+        console.error(error.message);
+      });
+  }
   _getTemplate() {
     return document
       .querySelector(this._cardSelector)
